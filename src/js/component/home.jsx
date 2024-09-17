b1296773ca221ea31d5d6cc733222fdd932718f6
diego <diegozaratean@gmail.com>
2024-09-17 01:23:41 +0000

    add react router

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import About from "./about";
+import Service from "./service";
+import Contact from "./contact";
 
-//create your first component
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./header";
 const Home = () => {
+	const [carrito, setCarrito] = useState('el carrito esta vacio')
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<About />} />
+					<Route path="/servicios" element={<Service />} />
+					<Route path="/contactanos" element={<Contact />} />
+				</Routes>
+				<p>{carrito}</p>
+				<button onClick={()=>setCarrito('agregaste 20 prodcutos')}>Agregar al carrito</button>
+				<h1 className="text-center mt-5">Footer</h1>
+			</BrowserRouter>
+		</>
 	);
 };
 
---
+++ src/js/component/service.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Service = () => {
+	return (
+		<>
+			<h1>Service</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas, necessitatibus amet provident dolorum fugit aut, reprehenderit autem cum odit quisquam ullam tenetur, ea accusamus facilis! Voluptates nemo ducimus quasi.
+
+                si quieres mas info  <a href="/contactanos">hablemos</a>
+
+                mira mi cv  <a target="_blanck" href="github.com">hablemos</a>
+            </p>
+		</>
+	);
+};
+
+export default Service;
